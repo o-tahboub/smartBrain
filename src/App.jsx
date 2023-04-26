1be8de1cd53e4443ad8e7ab892e61d3b6e476889
o-tahboub <o.tahboob2000@gmail.com>
2023-04-26 11:27:05 +0100

    Added methods as a draft for handling image detection form acitivity

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,30 @@ import ImageLinkForm from './components/ImageLinkForm'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = (event) => {
+    console.log(event);
+  }
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}/>
         {/* 
         <FaceRecognition /> */}
       </div>
--- src/components/ImageLinkForm.jsx
+++ src/components/ImageLinkForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className='center m-4 text-base'>
@@ -15,7 +15,10 @@ const ImageLinkForm = () => {
                                 rounded-lg 
                                 shadow-lg
                                 bg-sky-800'>
-                    <input type="text" className='text-sm p-2 w-9/12 mx-auto'/>
+                    <input 
+                        type="text" 
+                        className='text-sm p-2 w-9/12 mx-auto' 
+                        onChange={onInputChange}/>
                     <button className='w-3/12 
                         ease-out 
                         text-sm
@@ -27,7 +30,8 @@ const ImageLinkForm = () => {
                         inline-block
                         text-white
                         bg-purple-600
-                        cursor-pointer'>Detect</button>
+                        cursor-pointer'
+                        onClick={onButtonSubmit}>Detect</button>
                 </div>
             </div>
         </div>
