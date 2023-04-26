b7f8004acdf6e3ed4adf8fef544d04ac519764f6
o-tahboub <o.tahboob2000@gmail.com>
2023-04-26 12:44:59 +0100

    Added initial working setup for clarifai api

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,56 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: ''
+      input: '',
+      clarifaiConfig: {
+        PAT:***REMOVED***,
+        USER_ID:***REMOVED***,
+        APP_ID:***REMOVED***,
+        MODEL_ID: ***REMOVED***,
+        IMAGE_URL: 'https://samples.clarifai.com/metro-north.jpg'
+      }
     }
+  }
+
+  setupClarifiaRequestBody = () => {
+    const config = this.state.clarifaiConfig;
+    return JSON.stringify({
+      "user_app_id": {
+          "user_id": config.USER_ID ,
+          "app_id": config.APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": config.IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+  }
+
+  setupClarifiaRequestOptions = () => {
+    const config = this.state.clarifaiConfig;
+    return {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + config.PAT
+      },
+      body: this.setupClarifiaRequestBody(config)
+    };
+  }
+
+  setupClarifaiRequest = () => {
+    const config = this.state.clarifaiConfig;
+    const requestOptions = this.setupClarifiaRequestOptions(config);
+  
+    fetch("https://api.clarifai.com/v2/models/" + config.MODEL_ID + "/outputs", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   }
 
   onInputChange = (event) => {
@@ -18,7 +66,8 @@ class App extends Component {
   }
 
   onButtonSubmit = (event) => {
-    console.log(event);
+    const config = this.state.clarifaiConfig;
+    this.setupClarifaiRequest(config);
   }
 
   render() {
