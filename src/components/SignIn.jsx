4af60698d01d0a7f1485793e29343c26231fe25d
o-tahboub <o.tahboob2000@gmail.com>
2023-05-10 22:05:56 +0100

    created sign in component

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const SignIn = () => {
+    return(
+        <div className="center px-6 py-4 ">
+            <div className="w-full max-w-xs shadow-2xl">
+                <form className="rounded px-8 pt-6 pb-8 mb-4">
+                    <h2 className="text-white text-3xl mb-4">Sign In</h2>
+                    <div className="mb-4">
+                    <label className="block text-white text-base mb-2" htmlFor="username">
+                        Username
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+                    </div>
+                    <div className="mb-6">
+                    <label className="block text-white text-base mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                    <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                    </div>
+                    <div className="flex items-center justify-between">
+                    <button className="bg-purple-600 hover:bg-blue-800 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Sign In
+                    </button>
+                    <a className="inline-block align-baseline text-base text-white hover:text-blue-800" href="#">
+                        Register
+                    </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn
